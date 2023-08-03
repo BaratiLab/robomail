514922a0800ff5e2d9f17e864ef7cf860198722c
Alison Bartsch <abartsch@andrew.cmu.edu>
2023-08-03 11:50:13 -0400

    added typing annotations to joints

--- robomail/motion/live_controllers.py
+++ robomail/motion/live_controllers.py
@@ -1,9 +1,9 @@
 import numpy as np
 
-from frankapy import FrankaArm, SensorDataMessageType, FeedbackControllerType
+from frankapy import FrankaArm, SensorDataMessageType
 from frankapy import FrankaConstants as FC
 from frankapy.proto_utils import sensor_proto2ros_msg, make_sensor_group_msg
-from frankapy.proto import PosePositionSensorMessage, JointPositionSensorMessage, JointImpedanceFeedbackControllerMessage #, ShouldTerminateSensorMessage, CartesianImpedanceSensorMessage
+from frankapy.proto import PosePositionSensorMessage, JointPositionSensorMessage
 from franka_interface_msgs.msg import SensorDataGroup
 
 import rospy
@@ -12,9 +12,12 @@ import threading
 from copy import deepcopy
 import time
 
+from typing import List, Dict, Tuple
+
 class GotoPoseLive:
     def __init__(self, dt = 0.04, T = 1000, cartesian_impedances = None, step_size = 0.05):
         """
+        NOTE: This code has not been commented or riggerously tested yet. It works (I think), but be careful when using it!
         dt: the time inbetween each communication loop
         T: the total time the skill will be live for
         cartesian_impedances: list of cartesian impedaces. If none, uses default from FranaConstants
@@ -107,74 +110,68 @@ class GotoPoseLive:
 
 
 class GotoJointsLive:
-    def __init__(self, dt = 0.04, T = 1000, k_gains = None, d_gains = None, ignore_virtual_walls = False, max_speed = 0.5, max_rotation = 0.3):
+    def __init__(self, dt:float = 0.04, T:int = 1000, k_gains:List[float] = (0.5*np.array(FC.DEFAULT_K_GAINS)).tolist(), 
+                 d_gains:List[float] = (0.5*np.array(FC.DEFAULT_D_GAINS)).tolist(), ignore_virtual_walls:bool = False, 
+                 max_rotation:float = 0.3, max_speed:float = 0.5) -> None:
         """
         dt: the time inbetween each communication loop (0.04 is about the min you want to do)
         T: the total time the skill will be live for
-        k_gains: list of k gains for joint PID. I think. It's passed to fa.goto_joints.
-        d_gains: list of d gains for joint PID. I think. It's passed to fa.goto_joints.
-        max_speed: (rad/s) max rotation speed the controller will tell the robot to move in one step. If the goal rotation
-            is greater, then the rotation command sent to the controller will be cliped accordingly.
+        k_gains: list of k gains for joint PID. Defaults to 50% of the defualts listed in Franka Constants (I found this value worked well)
+        d_gains: list of d gains for joint PID. Defaults to 50% of the defualts listed in Franka Constants (I found this value worked well)
+        max_rotation: maximum rotation the arm will command one joint to move in one timestep. 
+        max_speed: (rad/s) If the commanded rotation is greated then max_rotation, then the skill will have the arm rotate at this speed.
         ignore_virtual_walls: ignores python virtual walls. We shouldn't need to do this but we do, for some reason.
         """
-        self.dt = dt
-        self.T = T
-
-        # my defaults for k and d. They seem to work OK for joint 6.
-        if d_gains == None:
-            self.d_gains = (0.5*np.array(FC.DEFAULT_D_GAINS)).tolist()
-        else:
-            self.d_gains = d_gains
-        if k_gains == None:
-            self.k_gains = (0.5*np.array(FC.DEFAULT_K_GAINS)).tolist()
-        else:
-            self.k_gains = k_gains
+        self.dt: float = dt
+        self.T: int = T
+        self.k_gains: List[float] = k_gains
+        self.d_gains: List[float] = d_gains
 
         self.fa = FrankaArm()
-        self.move_joints = self.fa.get_joints() # joints to send to the skill
-        self.goal_joints = deepcopy(self.move_joints) # joints the user provides
-        self.running = False # flag used to stop the loop
-        self.ignore_virtual_walls = ignore_virtual_walls
-        self.max_rot = max_rotation
-        self.max_speed = max_speed
+        self.move_joints: np.ndarray = self.fa.get_joints() # joints to send to the skill
+        self.goal_joints: np.ndarray = deepcopy(self.move_joints) # joints the user provides
+        self.running: bool = False # flag used to stop the loop
+        self.ignore_virtual_walls: bool = ignore_virtual_walls
+        self.max_rotation: float = max_rotation
+        self.max_speed: float = max_speed
 
     
-    def run(self):
-        rate = rospy.Rate(1/self.dt) #set the rospy rate.
-        pub = rospy.Publisher(FC.DEFAULT_SENSOR_PUBLISHER_TOPIC, SensorDataGroup, queue_size=10)
-        initialize = True #set initilize to true
-        id = 0 #id counter for the ross message
+    def run(self) -> None:  
+        rate = rospy.Rate(hz=1/self.dt) #set the rospy rate.
+        pub = rospy.Publisher(name=FC.DEFAULT_SENSOR_PUBLISHER_TOPIC, data_class=SensorDataGroup, queue_size=10)
+        initialize: bool = True #set initilize to true
+        id: int = 0 #id counter for the ross message
 
         # loop untill the 'running' flag is set to false
         while self.running:
-            # If the robot is trying to move more than max_rot, clip the motion.
-            cur_joints = self.fa.get_joints()
-            move_to_joints = self.get_move_to_joints()
-            delta_joints = move_to_joints - cur_joints
+            # If the robot is trying to move more than max_rotation, clip the motion.
+            cur_joints: np.ndarray = self.fa.get_joints()
+            move_to_joints:np.ndarray = self.get_move_to_joints()
+            delta_joints: np.ndarray = move_to_joints - cur_joints
             for i in range(len(delta_joints)):
-                if abs(delta_joints[i]) > self.max_rot:
-                    move_to_joints[i] = self.max_rot*np.sign(delta_joints[i]) + cur_joints[i]
+                if abs(delta_joints[i]) > self.max_rotation:
+                    move_to_joints[i] = self.max_rotation*np.sign(delta_joints[i]) + cur_joints[i]
                     print('violating max rotation')
 
             if initialize: # start the skill
-                self.fa.goto_joints(move_to_joints, duration=self.T, dynamic=True, ignore_virtual_walls=self.ignore_virtual_walls, 
+                self.fa.goto_joints(joints=move_to_joints, duration=self.T, dynamic=True, ignore_virtual_walls=self.ignore_virtual_walls, 
                                     k_gains=self.k_gains, d_gains=self.d_gains)
 
                 initialize = False
-                init_time = rospy.Time.now().to_time() # get initial time.
+                init_time: float = rospy.Time.now().to_time() # get initial time.
             
             else: # send update message
                 # print("update go to pose", self.pose.translation)
-                timestamp = rospy.Time.now().to_time() - init_time # update time stamp
+                timestamp: float = rospy.Time.now().to_time() - init_time # update time stamp
 
                 # ross messages:
                 traj_gen_proto_msg = JointPositionSensorMessage(
                     id=id, timestamp=timestamp,
                     joints = move_to_joints
                 )
-                ros_msg = make_sensor_group_msg(
+                ros_msg: SensorDataGroup = make_sensor_group_msg(
                     trajectory_generator_sensor_msg=sensor_proto2ros_msg(
-                        traj_gen_proto_msg, SensorDataMessageType.POSE_POSITION),
+                        sensor_proto_msg=traj_gen_proto_msg, sensor_data_type=SensorDataMessageType.POSE_POSITION),
                 )
 
                 # uncomment below to log the ross messages to the terminal
@@ -183,32 +180,32 @@ class GotoJointsLive:
             rate.sleep() # sleep for dt.
             id += 1
 
-    def get_goal_joints(self):
+    def get_goal_joints(self) -> np.ndarray:
         # Returns the goal joints
         return self.goal_joints
     
-    def set_goal_joints(self, goal_joints):
+    def set_goal_joints(self, goal_joints) -> None:
         # set the goal joints. This is used to control the robot.
         self.goal_joints = goal_joints
-        self.start_joints = self.fa.get_joints()
-        self.move_start_time = time.time()
+        self.start_joints: np.ndarray= self.fa.get_joints()
+        self.move_start_time: float = time.time()
 
         # If the goal joint is futher away then a single time step movign at the max speed,
         # we will need to interpolate the move_to commands
-        self.interpolate_move_to = np.any(abs(self.goal_joints-self.start_joints) > self.max_rot)
+        self.interpolate_move_to: bool = np.any(abs(self.goal_joints-self.start_joints) > self.max_rotation)
 
 
-    def get_move_to_joints(self):
+    def get_move_to_joints(self) -> np.ndarray:
         if self.interpolate_move_to:         
-            delta_joints = self.goal_joints - self.start_joints
-            max_move = (time.time()-self.move_start_time)*self.max_speed
-            motion_magnitude = np.minimum(np.abs(delta_joints), max_move) # eliment-wize minimum
+            delta_joints: np.ndarray = self.goal_joints - self.start_joints
+            max_move: float = (time.time()-self.move_start_time)*self.max_speed
+            motion_magnitude: np.ndarray = np.minimum(np.abs(delta_joints), max_move) # eliment-wize minimum
             return(self.start_joints + motion_magnitude*np.sign(delta_joints))
         else:
             return(self.goal_joints)
         
             
-    def start(self):
+    def start(self) -> None:
         # start run() in a new thread
         self.running = True
         self.thread = threading.Thread(target=self.run, daemon=True)
@@ -216,7 +213,7 @@ class GotoJointsLive:
         self.move_start_time = time.time()
 
     
-    def stop(self):
+    def stop(self) -> None:
         # stop runing the thread by setting the 'running' flag to false, then waiting for 
         # the tread to terminate. Finally, stop any ongoing skill.
         self.running = False
