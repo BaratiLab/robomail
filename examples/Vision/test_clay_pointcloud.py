c985298a212fb0510dc1574e2c8f3d02238b4622
Alison Bartsch <abartsch@andrew.cmu.edu>
2023-11-01 16:51:00 -0400

    added example clay fusal script

---
+++ examples/Vision/test_clay_pointcloud.py
@@ -0,0 +1,26 @@
+import time
+import numpy as np
+import open3d as o3d
+import robomail.vision as vis
+
+# initialize the cameras
+cam2 = vis.CameraClass(2)
+cam3 = vis.CameraClass(3)
+cam4 = vis.CameraClass(4)
+cam5 = vis.CameraClass(5)
+
+# initialize the 3D vision code
+pcl_vis = vis.Vision3D()
+
+# get the point clouds
+_, _, pc2, _ = cam2._get_next_frame()
+_, _, pc3, _ = cam3._get_next_frame()
+_, _, pc4, _ = cam4._get_next_frame()
+_, _, pc5, _ = cam5._get_next_frame()
+
+scaled_pcl = pcl_vis.fuse_point_clouds(pc2, pc3, pc4, pc5)
+
+# visualize the returned scaled point cloud
+pointcloud = o3d.geometry.PointCloud()
+pointcloud.points = o3d.utility.Vector3dVector(scaled_pcl)
+pointcloud.colors = o3d.utility.Vector3dVector(np.tile(np.array([0,0,1]), (len(scaled_pcl),1)))
