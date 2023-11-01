edeb6eeb76c3f9f614ce6d57d220e1f2d4232a09
Alison Bartsch <abartsch@andrew.cmu.edu>
2023-11-01 14:49:39 -0400

    finished example

--- examples/Vision/example_camera_class.py
+++ examples/Vision/example_camera_class.py
@@ -1,3 +1,4 @@
+import cv2
 import numpy as np
 import robomail.vision as vis
 
@@ -7,8 +8,19 @@ Example script demonstrating how to call camera wrapper to simplify using the ca
 
 # initialize the end effector camera
 cam1 = vis.CameraClass(1)
-cam_intrinsics  = cam1._get_real
+
+# functions you can call to get camera information
+cam_intrinsics  = cam1._get_cam_intrinsics()
+cam_extrinsics = cam1._get_cam_extrinsics()
+cam_number = cam1._get_cam_number()
+cam_serial = cam1._get_cam_serial()
 
 # begin loop to get images from camera
 while True:
     img, depth, pointcloud, verts = cam1._get_next_frame()
+
+    # CAN PUT ANYTHING HERE TO PROCESS THE IMAGES/DEPTH/POINTCLOUD
+
+    # visualize the camera frames
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
