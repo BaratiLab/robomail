019297ff9833f92d238e3425bc652f899873a294
Alison Bartsch <abartsch@andrew.cmu.edu>
2023-11-01 17:40:30 -0400

    added something

--- camera_calibration_scripts/two_camera_registration.py
+++ camera_calibration_scripts/two_camera_registration.py
@@ -1,4 +1,7 @@
+import time
 import numpy as np
+import open3d as o3d
+import robomail.vision as vis
 
 """
 This script is the working calibration code to get the transformation matrix between two cameras. Detailed instructions
@@ -34,3 +37,58 @@ CALIBRATION PARAMETER NOTES:
     - background removal radius: 1.65m
     - ensure calibration object facing both cameras with the same face as much as possible
 """
+
+cama = 2
+camb = 3
+
+# initialize the cameras
+camera_a = vis.CameraClass(cama)
+camera_b = vis.CameraClass(camb)
+
+# get the point clouds
+_, _, pca, _ = camera_a._get_next_frame()
+_, _, pcb, _ = camera_b._get_next_frame()
+
+# combine into single pointcloud
+pcda = o3d.geometry.PointCloud()
+pcda.points = pca.points
+pcda.colors = pca.colors
+
+pcdb = o3d.geometry.PointCloud()
+pcdb.points = pcb.points
+pcdb.colors = pcb.colors
+
+# remove the background
+pcda = remove_background(pcda, radius=0.9)
+pcdb = remove_background(pcdb, radius=0.9)
+
+# collect addition point clouds to combine
+for i in range(9):
+    _, _, pca, _ = camera_a._get_next_frame()
+    _, _, pcb, _ = camera_b._get_next_frame()
+    
+    # combine into single pointcloud
+    pcda.points.extend(pca.points)
+    pcda.colors.extend(pca.colors)
+    pcdb.points.extend(pcb.points)
+    pcdb.colors.extend(pcb.colors)
+
+pcda, ind = pcda.remove_statistical_outlier(nb_neighbors=20, std_ratio=2.0)
+pcdb, ind = pcdb.remove_statistical_outlier(nb_neighbors=20, std_ratio=2.0)
+
+# remove the background
+pcda = remove_background(pcda, radius=1.65) # 0.9
+pcdb = remove_background(pcdb, radius=1.65) # 0.9
+
+# RANSAC registration
+voxel_size = 0.01 # 0.018 # 0.005 # 0.001 # 0.025 # in meters 
+source, target, source_down, target_down, source_fpfh, target_fpfh = prepare_dataset(voxel_size, pcda, pcdb)
+result_ransac = execute_global_registration(source_down, target_down, source_fpfh, target_fpfh, voxel_size)
+
+# ICP finetuning
+result_icp = refine_registration(source, target, voxel_size, result_ransac.transformation)
+cama_to_camb = result_icp.transformation
+pcda.transform(cama_to_camb)
+o3d.visualization.draw_geometries([pcda, pcdb])
+
+np.save("live_registration/RANSAC/transform_cam" + str(cama) + "_to_cam" + str(camb) + ".npy", cama_to_camb)
--- robomail/vision/__init__.py
+++ robomail/vision/__init__.py
@@ -2,3 +2,4 @@ from .simple_vision import GetPixelPose, GetPatchPose
 from .camera import CameraClass
 from .calibration_vision import CalibrationClass
 from .pointcloud_vision import Vision3D
+from .cam_utils import get_cam_info
