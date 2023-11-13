ad0eac6bb4f9b79b142fdfa58f212b82b1625a3e
charlotteavra <charlotteavra02@gmail.com>
2023-11-13 12:49:17 -0500

    update docstrings

--- robomail/vision/calibration_vision.py
+++ robomail/vision/calibration_vision.py
@@ -2,35 +2,22 @@ import copy
 import numpy as np
 import open3d as o3d
 
-class CalibrationClass():
+
+class CalibrationClass:
     def __init__(self):
         pass
 
-    def prepare_dataset(self, voxel_size, source, target, transform=np.identity(4)):
-        """
-        1. Accepts voxel size and 2 loaded pointclouds: the source and target respectively
-        2. Reads input .ply files and defines source and target point clouds
-        3. Removes statistical outliers and crops incoming pointclouds using remove_outliers and remove_background 
-        4. Visualizes raw source and target point clouds in initial configuration (transform = np.identity(4))
-        5. Downsamples point clouds and calculates FPFH's using preprocess_point_cloud
-        6. Returns source, target, downsampled source and target, and source and target FPFH's 
-        """
-        
-        print("\nDisplaying raw alignment...")
-        self.draw_registration_result(source, target, transform)
-
-        source_down, source_fpfh = self.preprocess_point_cloud(source, voxel_size * 3) # compute FPFH and downsampled point cloud for source and target 
-        target_down, target_fpfh = self.preprocess_point_cloud(target, voxel_size * 3)
-
-        return source, target, source_down, target_down, source_fpfh, target_fpfh
-
     def draw_registration_result(self, source, target, transformation):
         """
-        Visualizes a transformed COPY of the source point cloud together with the original target point cloud 
-        
-        **Note** This function does not apply a transform to the original source or target pointclouds
-        """  
-        
+        Visualizes a transformed COPY of the source point cloud together with the original target point cloud
+        NOTE: This function does not apply a transform to the original source or target pointclouds
+
+        Args:
+        source (o3d.geometry.PointCloud): source point cloud
+        target (o3d.geometry.PointCloud): target point cloud
+        transformation (npt.NDArray): transformation matrix of size (4,4)
+        """
+
         source_temp = copy.deepcopy(source)
         target_temp = copy.deepcopy(target)
         source_temp.transform(transformation)
@@ -38,96 +25,152 @@ class CalibrationClass():
 
     def preprocess_point_cloud(self, pcd, voxel_size):
         """
-        1. Downsample the point cloud
-        2. Estimate normals
-        3. Compute a FPFH feature (a 33-dimensional vector that describes the local geometric property of a point) for each point 
+        Downsamples input point cloud based on a given voxel size and computes FPFH feature
+
+        Args:
+        pcd (o3d.geometry.PointCloud): input point cloud
+        voxel_size (float): voxel size to downsample into
+
+        Returns:
+        pcd_down (o3d.geometry.PointCloud): output downsampled point cloud
+        pcd_fpfh (open3d.registration.Feature): FPFH feature of input point cloud
         """
         print("\nDownsampling with a voxel size %.3f..." % voxel_size)
-        pcd_down = pcd.voxel_down_sample(voxel_size * 0.5) # global registration is only performed on a heavily down-sampled point clouds
+        pcd_down = pcd.voxel_down_sample(
+            voxel_size * 0.5
+        )  # global registration is only performed on a heavily down-sampled point clouds
 
         radius_normal = voxel_size * 2
         print("\nEstimating normal with search radius %.3f..." % radius_normal)
         pcd_down.estimate_normals(
-            o3d.geometry.KDTreeSearchParamHybrid(radius=radius_normal, max_nn=30))
+            o3d.geometry.KDTreeSearchParamHybrid(radius=radius_normal, max_nn=30)
+        )
 
         radius_feature = voxel_size * 5
         print("\nComputing FPFH feature with search radius %.3f...  " % radius_feature)
         pcd_fpfh = o3d.pipelines.registration.compute_fpfh_feature(
             pcd_down,
-            o3d.geometry.KDTreeSearchParamHybrid(radius=radius_feature, max_nn=100))
+            o3d.geometry.KDTreeSearchParamHybrid(radius=radius_feature, max_nn=100),
+        )
 
         return pcd_down, pcd_fpfh
 
     def prepare_dataset(self, voxel_size, source, target, transform=np.identity(4)):
         """
-        1. Accepts voxel size and 2 loaded pointclouds: the source and target respectively
-        2. Reads input .ply files and defines source and target point clouds
-        3. Removes statistical outliers and crops incoming pointclouds using remove_outliers and remove_background 
-        4. Visualizes raw source and target point clouds in initial configuration (transform = np.identity(4))
-        5. Downsamples point clouds and calculates FPFH's using preprocess_point_cloud
-        6. Returns source, target, downsampled source and target, and source and target FPFH's 
+        Preprocesses source and target point clouds using preprocess_point_cloud()
+
+        Args:
+        voxel_size (float): voxel size to downsample into
+        source (o3d.geometry.PointCloud): source point cloud
+        target (o3d.geometry.PointCloud): target point cloud
+        transform (npt.NDArray): transformation matrix of size (4,4), default identity matrix (no transformation)
+
+        Returns:
+        source (o3d.geometry.PointCloud): source point cloud
+        target (o3d.geometry.PointCloud): target point cloud
+        source_down (o3d.geometry.PointCloud): downsampled source point cloud
+        target_down (o3d.geometry.PointCloud): downsampled target point cloud
+        source_fpfh (open3d.registration.Feature): FPFH feature of source point cloud
+        target_fpfh (open3d.registration.Feature): FPFH feature of target point cloud
         """
-        
+
         print("\nDisplaying raw alignment...")
         self.draw_registration_result(source, target, transform)
 
-        source_down, source_fpfh = self.preprocess_point_cloud(source, voxel_size * 3) # compute FPFH and downsampled point cloud for source and target 
+        source_down, source_fpfh = self.preprocess_point_cloud(
+            source, voxel_size * 3
+        )  # compute FPFH and downsampled point cloud for source and target
         target_down, target_fpfh = self.preprocess_point_cloud(target, voxel_size * 3)
 
         return source, target, source_down, target_down, source_fpfh, target_fpfh
 
-    def execute_global_registration(self, source_down, target_down, source_fpfh, target_fpfh, voxel_size):
+    def execute_global_registration(
+        self, source_down, target_down, source_fpfh, target_fpfh, voxel_size
+    ):
         """
-        Applies RANSAC global registration method 
+        Applies RANSAC global registration method to align point clouds from different cameras
+
+        Args:
+        source_down (o3d.geometry.PointCloud): downsampled source point cloud from prepare_dataset()
+        target_down (o3d.geometry.PointCloud): downsampled target point cloud from prepare_dataset()
+        source_fhfh (o3d.registration.Feature): FPFH feature for each point of source point cloud
+        target_fhfh (o3d.registration.Feature): FPFH feature for each point of target point cloud
+        voxel_size (float): voxel size used to define distance threshold for alignment
+
+        Returns:
+        result (o3d.pipelines.registration.RegistrationResult): output of Open3D global RANSAC registration function
         """
-        distance_threshold = voxel_size * 1.5 # define maximum correspondence distance
+        distance_threshold = voxel_size * 1.5  # define maximum correspondence distance
         print("\nGlobal registration with RANSAC on downsampled point clouds...")
         print("\nDownsampling voxel size: %.3f," % voxel_size)
         print("\nDistance threshold: %.3f." % distance_threshold)
-        
-        result = o3d.pipelines.registration.registration_ransac_based_on_feature_matching(
-            source = source_down, 
-            target = target_down, 
-            source_feature = source_fpfh, 
-            target_feature = target_fpfh,
-            mutual_filter = True,
-            max_correspondence_distance = distance_threshold,
-            estimation_method = o3d.pipelines.registration.TransformationEstimationPointToPoint(True),
-            ransac_n = 4,
-            checkers = [o3d.pipelines.registration.CorrespondenceCheckerBasedOnEdgeLength(0.9),
-                o3d.pipelines.registration.CorrespondenceCheckerBasedOnDistance(distance_threshold)], 
-            criteria = o3d.pipelines.registration.RANSACConvergenceCriteria(max_iteration = 4000000, confidence = 1.0))
 
-        # visualize 
+        result = o3d.pipelines.registration.registration_ransac_based_on_feature_matching(
+            source=source_down,
+            target=target_down,
+            source_feature=source_fpfh,
+            target_feature=target_fpfh,
+            mutual_filter=True,
+            max_correspondence_distance=distance_threshold,
+            estimation_method=o3d.pipelines.registration.TransformationEstimationPointToPoint(
+                True
+            ),
+            ransac_n=4,
+            checkers=[
+                o3d.pipelines.registration.CorrespondenceCheckerBasedOnEdgeLength(0.9),
+                o3d.pipelines.registration.CorrespondenceCheckerBasedOnDistance(
+                    distance_threshold
+                ),
+            ],
+            criteria=o3d.pipelines.registration.RANSACConvergenceCriteria(
+                max_iteration=4000000, confidence=1.0
+            ),
+        )
+
+        # visualize
         print("\nDisplaying alignment after RANSAC global registration...")
         self.draw_registration_result(source_down, target_down, result.transformation)
-        
+
         return result
 
     def refine_registration(self, source, target, voxel_size, trans_global):
         """
-        Refine global registraction with local refinement (ICP) 
+        Applies iterative-closest point algorithm (ICP) to refine global registration
+        NOTE: Global registration should be run on source and target point clouds before using
+        this function
+
+        Args:
+        source (o3d.geometry.PointCloud): source point cloud
+        target (o3d.geometry.PointCloud): target point cloud
+        voxel_size (float): voxel size used to define distance threshold for alignment
+        trans_global (npt.NDArray): result of global registration, matrix of size (4,4), can use result.transformation
+
+        Returns:
+        result (o3d.pipelines.registration.RegistrationResult): output of Open3D ICP registration function
         """
-        distance_threshold = voxel_size * 0.5 # define maximum correspondence distance
-        
+        distance_threshold = voxel_size * 0.5  # define maximum correspondence distance
+
         # calculate global registration evaluation
         print("\nGlobal registration alignment evaluation:")
-        evaluation = o3d.pipelines.registration.evaluate_registration(source, target, distance_threshold, trans_global)
+        evaluation = o3d.pipelines.registration.evaluate_registration(
+            source, target, distance_threshold, trans_global
+        )
         print(evaluation)
-        
+
         print("\nPoint-to-plane ICP registration is applied on original point")
         print("   clouds to refine the alignment. This time we use a strict")
         print("   distance threshold %.3f." % distance_threshold)
 
         result = o3d.pipelines.registration.registration_icp(
-            source = source, 
-            target = target, 
-            max_correspondence_distance = distance_threshold, 
-            init = trans_global,
-            estimation_method = o3d.pipelines.registration.TransformationEstimationPointToPoint())
-            #criteria = o3d.pipelines.registration.ICPConvergenceCriteria(max_iteration=10))
+            source=source,
+            target=target,
+            max_correspondence_distance=distance_threshold,
+            init=trans_global,
+            estimation_method=o3d.pipelines.registration.TransformationEstimationPointToPoint(),
+        )
+        # criteria = o3d.pipelines.registration.ICPConvergenceCriteria(max_iteration=10))
 
-        # visualize 
+        # visualize
         print("\nDisplaying alignment after local refinement...")
         self.draw_registration_result(source, target, result.transformation)
 
--- robomail/vision/pointcloud_vision.py
+++ robomail/vision/pointcloud_vision.py
@@ -1,18 +1,20 @@
 import open3d as o3d
 import copy
 import numpy as np
+import numpy.typing as npt
 from .cam_utils import get_cam_info
 from skimage.color import rgb2lab
 from matplotlib import pyplot as plt
 from shapely.geometry import Point, Polygon
 
-class Vision3D():
+
+class Vision3D:
     def __init__(self):
         # save a dictionary of the camera transforms (should be from each static camera to world frame)
         self.camera_transforms = {}
-        for i in range(2,6):
+        for i in range(2, 6):
             _, transform, _ = get_cam_info(i)
-            self.camera_transforms[i] = transform# .matrix()
+            self.camera_transforms[i] = transform  # .matrix()
             # _, self.camera_transforms[i], _ = get_cam_info(i)
 
     def plot_target_and_state_clouds(self, state, target):
@@ -21,23 +23,31 @@ class Vision3D():
         """
         target_pcl = o3d.geometry.PointCloud()
         target_pcl.points = o3d.utility.Vector3dVector(target)
-        target_colors = np.tile(np.array([1,0,0]), (len(target),1))
+        target_colors = np.tile(np.array([1, 0, 0]), (len(target), 1))
         target_pcl.colors = o3d.utility.Vector3dVector(target_colors)
 
         pcl = o3d.geometry.PointCloud()
         pcl.points = o3d.utility.Vector3dVector(state)
-        pcl_colors = np.tile(np.array([0,0,1]), (len(state),1))
+        pcl_colors = np.tile(np.array([0, 0, 1]), (len(state), 1))
         pcl.colors = o3d.utility.Vector3dVector(pcl_colors)
         return target_pcl, pcl
-    
+
     def pcl_to_image(self, pcd1, pcd2, save_path):
         """
+        Saves image on Open3D visualizer as PNG
+
+        Args:
+        pcd1 (o3d.geometry.PointCloud): input point cloud to visualize
+        pcd2 (Optional[o3d.geometry.PointCloud]): additional input point cloud to visualize
+        save_path (string): location to save PNG
         """
         vis = o3d.visualization.Visualizer()
         vis.create_window()
         vis.add_geometry(pcd1)
-        if pcd2 != False:
+
+        if pcd2:
             vis.add_geometry(pcd2)
+
         vis.run()
         image = vis.capture_screen_float_buffer(True)
         plt.imsave(save_path, np.asarray(image))
@@ -45,25 +55,45 @@ class Vision3D():
         del vis
 
     def remove_stage_grippers(self, pcd):
+        """
+        Removes points from input point cloud based on a constant Z threshold to remove
+        the clay stage and robot grippers
+
+        Args:
+        pcd (o3d.geometry.PointCloud): input point cloud
+
+        Returns:
+        pcd (o3d.geometry.PointCloud): output point cloud with clay stage and grippers removed
+        """
         points = np.asarray(pcd.points)
         colors = np.asarray(pcd.colors)
 
-        ind_z_upper = np.where(points[:,2] > 0.207)
+        ind_z_upper = np.where(points[:, 2] > 0.207)
         pcd.points = o3d.utility.Vector3dVector(points[ind_z_upper])
         pcd.colors = o3d.utility.Vector3dVector(colors[ind_z_upper])
 
         points = np.asarray(pcd.points)
         colors = np.asarray(pcd.colors)
-        ind_z_lower = np.where(points[:,2] < 0.27)
+        ind_z_lower = np.where(points[:, 2] < 0.27)
         pcd.points = o3d.utility.Vector3dVector(points[ind_z_lower])
         pcd.colors = o3d.utility.Vector3dVector(colors[ind_z_lower])
+
         return pcd
 
-    def remove_background(self, pcd, radius=0.9, center = np.array([0, 0, 0])):
+    def remove_background(
+        self, pcd, radius: float = 0.9, center: npt.NDArray = np.array([0, 0, 0])
+    ):
         """
-        1. Accept raw point cloud or point cloud with outliers removed
-        2. Crop points based on defined sphere parameters
-        3. Return cropped point cloud
+        Removes points from intput point cloud outside of a sphere of given radius
+        and center
+
+        Args:
+        pcd (o3d.geometry.PointCloud): input point cloud
+        radius (float): radius of sphere outside of which to remove points
+        center (np.array): coordinate of center of sphere outside of which to remove points
+
+        Returns:
+        pcd (o3d.geometry.PointCloud): output point cloud with points removed
         """
         points = np.asarray(pcd.points)
         colors = np.asarray(pcd.colors)
@@ -73,46 +103,62 @@ class Vision3D():
 
         pcd.points = o3d.utility.Vector3dVector(points[indices])
         pcd.colors = o3d.utility.Vector3dVector(colors[indices])
-        
+
         return pcd
 
     def lab_color_crop(self, pcd_incoming):
         """
-        1. Creates copy of incoming pcd so as to not permanently alter original
-        2. Defines vertices and color values of pcd copy
-        3. Converts colors form RBG to LAB space
-        4. Defines color threshold and applies threshold to verticies
-        5. Returns thresholded pcd 
+        Removes points from input point cloud based on color in LAB color space
+        (currently thresholds for a green color)
+
+        Args:
+        pcd_incoming (o3d.geometry.PointCloud): input point cloud
+
+        Returns:
+        pcd (o3d.geometry.PointCloud): output point cloud with points removed
         """
-        
+
         pcd = copy.deepcopy(pcd_incoming)
-        
+
         points = np.asarray(pcd.points)
         colors = np.asarray(pcd.colors)
-        lab_colors = rgb2lab(colors)
+        lab_colors = rgb2lab(colors)  # covert colors to LAB color space
 
-        a_idx = np.where(lab_colors[:,1]<-5)
-        l_idx = np.where(lab_colors[:,0]>0)
-        b_idx = np.where(lab_colors[:,2]>-5)
+        a_idx = np.where(lab_colors[:, 1] < -5)
+        l_idx = np.where(lab_colors[:, 0] > 0)
+        b_idx = np.where(lab_colors[:, 2] > -5)
 
         indices = np.intersect1d(a_idx, np.intersect1d(l_idx, b_idx))
 
         pcd.points = o3d.utility.Vector3dVector(points[indices])
         pcd.colors = o3d.utility.Vector3dVector(colors[indices])
-        
+
         return pcd
-    
+
     def fuse_point_clouds(self, pc2, pc3, pc4, pc5, vis=False):
         """
-        Updated fusal based on Charlotte's improvements
-        """    
-        # transform the clouds
+        Stitches together 4 point clouds from cameras 2, 3, 4, and 5 in the
+        Franka workspace into world coordinate frame and performs background
+        and color cropping
+
+        Args:
+        pc2 (o3d.geometry.PointCloud): point cloud from camera 2
+        pc3 (o3d.geometry.PointCloud): point cloud from camera 3
+        pc4 (o3d.geometry.PointCloud): point cloud from camera 4
+        pc5 (o3d.geometry.PointCloud): point cloud from camera 5
+        vis (bool): if True, will display final results (default False)
+
+        Returns:
+        rescaled_points (o3d.geometry.PointCloud): stitched and cropped final
+        point cloud scaled up by 10 (shapenet size) TODO
+        """
+        # transform each cloud to world frame
         pc2.transform(self.camera_transforms[2])
         pc3.transform(self.camera_transforms[3])
         pc4.transform(self.camera_transforms[4])
         pc5.transform(self.camera_transforms[5])
 
-        # combine the clouds
+        # combine the point clouds
         pointcloud = o3d.geometry.PointCloud()
         pointcloud.points = pc5.points
         pointcloud.colors = pc5.colors
@@ -123,76 +169,81 @@ class Vision3D():
         pointcloud.points.extend(pc4.points)
         pointcloud.colors.extend(pc4.colors)
 
-        # o3d.visualization.draw_geometries([pointcloud])
-
-        # ------ cropping point cloud ------ 
-        pointcloud, ind = pointcloud.remove_statistical_outlier(nb_neighbors=20, std_ratio=2.0) # remove statistical outliers                       
+        # crop point cloud
+        pointcloud, ind = pointcloud.remove_statistical_outlier(
+            nb_neighbors=20, std_ratio=2.0
+        )  # remove statistical outliers
         pointcloud = self.remove_stage_grippers(pointcloud)
-        # o3d.visualization.draw_geometries([pointcloud])
-        pointcloud = self.remove_background(pointcloud, radius = .15, center = np.array([0.6, -0.05, 0.3]))
-        # o3d.visualization.draw_geometries([pointcloud])
+        pointcloud = self.remove_background(
+            pointcloud, radius=0.15, center=np.array([0.6, -0.05, 0.3])
+        )
 
-        # ----- color thresholding -----
+        # color thresholding
         pointcloud = self.lab_color_crop(pointcloud)
-        # o3d.visualization.draw_geometries([pointcloud])
-        pointcloud, ind = pointcloud.remove_statistical_outlier(nb_neighbors=20, std_ratio=2.0)
+        pointcloud, ind = pointcloud.remove_statistical_outlier(
+            nb_neighbors=20, std_ratio=2.0
+        )
 
-        # ----- calculate point cloud normals -----
+        # get shape of clay base
         pointcloud.estimate_normals()
-
-        # ------ downsample the point cloud -------
         downpdc = pointcloud.voxel_down_sample(voxel_size=0.0025)
-        downpdc_points= np.asarray(downpdc.points)
-        # print("min: ", np.amin(downpdc_points, axis=0))
-        # print("max: ", np.amax(downpdc_points, axis=0))
-
-        # ----- get shape of clay base --------
+        downpdc_points = np.asarray(downpdc.points)
         # polygon_indices = np.where(downpdc_points[:,2] < 0.236) # PREVIOUS BEFORE 8/29
-        polygon_indices = np.where(downpdc_points[:,2] < 0.22)
+        polygon_indices = np.where(downpdc_points[:, 2] < 0.22)
 
         # polygon_indices = np.where(downpdc_points[:,2] < 0.234)
         polygon_pcl = o3d.geometry.PointCloud()
         polygon_pcl.points = o3d.utility.Vector3dVector(downpdc_points[polygon_indices])
 
-        # ------ generate a 2d grid of points for the base ---------
+        # generate a 2d grid of points for the base
         base_plane = []
-        minx, maxx = np.amin(downpdc_points[:,0]), np.amax(downpdc_points[:,0])
-        miny, maxy = np.amin(downpdc_points[:,1]), np.amax(downpdc_points[:,1])
-        minz, maxz = np.amin(downpdc_points[:,2]), np.amax(downpdc_points[:,2])
+        minx, maxx = np.amin(downpdc_points[:, 0]), np.amax(downpdc_points[:, 0])
+        miny, maxy = np.amin(downpdc_points[:, 1]), np.amax(downpdc_points[:, 1])
+        minz, maxz = np.amin(downpdc_points[:, 2]), np.amax(downpdc_points[:, 2])
         x_vals = np.linspace(minx, maxx, 50)
         y_vals = np.linspace(miny, maxy, 50)
-        xx, yy = np.meshgrid(x_vals, y_vals) # create grid that covers full area of 2d polygon  
+        xx, yy = np.meshgrid(
+            x_vals, y_vals
+        )  # create grid that covers full area of 2d polygon
         # z = 0.234 # height of the clay base
 
         z = 0.21
         # z = 0.232 # PREVIOUS BEFORE 8/29
-        zz = np.ones(len(xx.flatten()))*z
+        zz = np.ones(len(xx.flatten())) * z
         points = np.vstack((xx.flatten(), yy.flatten(), zz)).T
 
         grid_cloud = o3d.geometry.PointCloud()
         grid_cloud.points = o3d.utility.Vector3dVector(points)
 
-        # -------- crop shape of clay base out of 2d grid of points ---------
-        polygon_coords = np.asarray(polygon_pcl.points)[:,0:2]
+        # crop shape of clay base out of 2d grid of points
+        polygon_coords = np.asarray(polygon_pcl.points)[:, 0:2]
         polygon = Polygon(polygon_coords)
-        mask = [polygon.contains(Point(x, y)) for x, y in np.asarray(grid_cloud.points)[:,0:2]]
-        cropped_grid = np.asarray(grid_cloud.points)[:,0:2][mask]
-        zs = np.ones(len(cropped_grid))*z
-        cropped_grid = np.concatenate((cropped_grid, np.expand_dims(zs, axis=1)), axis=1)
+        mask = [
+            polygon.contains(Point(x, y))
+            for x, y in np.asarray(grid_cloud.points)[:, 0:2]
+        ]
+        cropped_grid = np.asarray(grid_cloud.points)[:, 0:2][mask]
+        zs = np.ones(len(cropped_grid)) * z
+        cropped_grid = np.concatenate(
+            (cropped_grid, np.expand_dims(zs, axis=1)), axis=1
+        )
 
         base_cloud = o3d.geometry.PointCloud()
         base_cloud.points = o3d.utility.Vector3dVector(cropped_grid)
 
-        # -------- add top part of clay to new base ------------
+        # add top part of clay to new base
         base_cloud.points.extend(downpdc.points)
-        cropped_plane, ind = base_cloud.remove_statistical_outlier(nb_neighbors=20, std_ratio=2.0)
+        cropped_plane, ind = base_cloud.remove_statistical_outlier(
+            nb_neighbors=20, std_ratio=2.0
+        )
 
-        base_cloud.colors = o3d.utility.Vector3dVector(np.tile(np.array([0,0,1]), (len(base_cloud.points),1)))
-        # o3d.visualization.draw_geometries([base_cloud])
+        base_cloud.colors = o3d.utility.Vector3dVector(
+            np.tile(np.array([0, 0, 1]), (len(base_cloud.points), 1))
+        )
 
         # uniformly sample 2048 points from each point cloud
         points = np.asarray(base_cloud.points)
-        idxs = np.random.randint(0,len(points),size=2048)
+        idxs = np.random.randint(0, len(points), size=2048)
         points = points[idxs]
 
         # re-process the processed_pcl to center
@@ -201,11 +252,15 @@ class Vision3D():
         centered_points = points - pc_center
 
         scale = 10
-        rescaled_points = scale*centered_points
+        rescaled_points = scale * centered_points
 
         pointcloud = o3d.geometry.PointCloud()
         pointcloud.points = o3d.utility.Vector3dVector(rescaled_points)
-        pointcloud.colors = o3d.utility.Vector3dVector(np.tile(np.array([0,0,1]), (len(rescaled_points),1)))
+        pointcloud.colors = o3d.utility.Vector3dVector(
+            np.tile(np.array([0, 0, 1]), (len(rescaled_points), 1))
+        )
+
         if vis:
             o3d.visualization.draw_geometries([pointcloud])
+
         return rescaled_points
