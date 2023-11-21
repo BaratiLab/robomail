eb206116527a49c9f1a6195f2a8043087cc36c04
Abraham <aigeorge@andrew.cmu.edu>
2023-11-21 12:30:33 -0500

    Updated threaded camera class to take in list inputs for camera parameters

--- robomail/vision/threaded_cameras.py
+++ robomail/vision/threaded_cameras.py
@@ -1,5 +1,6 @@
 from .camera import CameraClass
 import threading
+import numpy as np
 
 class ThreadedCameras:
     """
@@ -12,22 +13,41 @@ class ThreadedCameras:
         Initializes ThreadedCameras.
 
         :param cam_numbers: List of camera numbers to initialize CameraClass instances.
-        :param image_height: Height of the captured image. Defaults to 480.
-        :param image_width: Width of the captured image. Defaults to 848.
-        :param get_point_cloud: Flag to retrieve point cloud data. Defaults to False.
-        :param get_verts: Flag to retrieve vertices data. Defaults to False.
+        :param image_height: Height of the captured image. Defaults to 480. Either int (all cameras have same height) or list (each camera has its own height).
+        :param image_width: Width of the captured image. Defaults to 848. Either int (all cameras have same width) or list (each camera has its own width).
+        :param get_point_cloud: Flag to retrieve point cloud data. Defaults to False. Either bool (all cameras have same flag) or list (each camera has its own flag).
+        :param get_verts: Flag to retrieve vertices data. Defaults to False. Either bool (all cameras have same flag) or list (each camera has its own flag).
         """
         self.cameras = []
-        for cam_number in cam_numbers:
-            self.cameras.append(CameraClass(cam_number, H=image_height, W=image_width))
+        if isinstance(image_height, list) or isinstance(image_height, np.ndarray):
+            assert len(image_height) == len(cam_numbers), "image_height must be either an int or a list of length len(cam_numbers)"
+        else:
+            image_heights = [image_height for _ in range(len(cam_numbers))]
+        
+        if isinstance(image_width, list) or isinstance(image_width, np.ndarray):
+            assert len(image_width) == len(cam_numbers), "image_width must be either an int or a list of length len(cam_numbers)"
+        else:
+            image_widths = [image_width for _ in range(len(cam_numbers))]
+        
+        if isinstance(get_point_cloud, list) or isinstance(get_point_cloud, np.ndarray):
+            assert len(get_point_cloud) == len(cam_numbers), "get_point_cloud must be either a bool or a list of length len(cam_numbers)"
+        else:
+            self.get_point_cloud = [get_point_cloud for _ in range(len(cam_numbers))]
+        
+        if isinstance(get_verts, list) or isinstance(get_verts, np.ndarray):
+            assert len(get_verts) == len(cam_numbers), "get_verts must be either a bool or a list of length len(cam_numbers)"
+        else:
+            self.get_verts = [get_verts for _ in range(len(cam_numbers))]
+
+        
+        for i, cam_number in enumerate(cam_numbers):
+            self.cameras.append(CameraClass(cam_number, H=image_heights[i], W=image_widths[i]))
         self._image_data = [None for _ in range(len(cam_numbers))]
         self._image_lock = threading.Lock()  # Lock for when self._image_data is being altered
         self.new_image_event = threading.Event()  # Event to signal when a new image is available
         self.record = False  # Flag to determine whether to record images
         self._thread = threading.Thread(target=self._run, daemon=True)
         self._thread.start()
-        self.get_point_cloud = get_point_cloud
-        self.get_verts = get_verts
 
         
     def _run(self):
@@ -38,7 +58,7 @@ class ThreadedCameras:
             new_data = [None for _ in range(len(self.cameras))]
 
             for i, camera in enumerate(self.cameras):
-                new_data[i] = camera.get_next_frame(get_point_cloud=self.get_point_cloud, get_verts=self.get_verts)
+                new_data[i] = camera.get_next_frame(get_point_cloud=self.get_point_cloud[i], get_verts=self.get_verts[i])
 
             with self._image_lock:
                 # self._image_data now points to the recently collected data
@@ -71,5 +91,5 @@ class ThreadedCameras:
         """Stops the thread and joins it, ending the frame capturing process."""
         self.record = False
         self._thread.join()
-        print("thread joined")
+        print("camera thread joined")
         return
