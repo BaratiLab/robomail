b6934e003c752f5cafa2f6001f4708d8f10d8e8b
alison-bartsch <abartsch@andrew.cmu.edu>
2024-12-02 16:04:54 -0500

    made key modifications for clay handling

--- robomail/vision/pointcloud_vision.py
+++ robomail/vision/pointcloud_vision.py
@@ -80,7 +80,7 @@ class Vision3D:
         # removing the grippers
         points = np.asarray(pcd.points)
         colors = np.asarray(pcd.colors)
-        ind_z_lower = np.where(points[:, 2] < 0.2)
+        ind_z_lower = np.where(points[:, 2] < 0.15)
         pcd.points = o3d.utility.Vector3dVector(points[ind_z_lower])
         pcd.colors = o3d.utility.Vector3dVector(colors[ind_z_lower])
 
@@ -139,7 +139,7 @@ class Vision3D:
             l_idx = np.where(lab_colors[:, 0] > 0) # 0
             b_idx = np.where(lab_colors[:, 2] < -5)
         else:
-            raise ValueError("Invalid color")
+            raise ValueError("Invalid color, needs to be implemented!")
 
         indices = np.intersect1d(a_idx, np.intersect1d(l_idx, b_idx))
 
@@ -241,6 +241,149 @@ class Vision3D:
             pointcloud, radius=0.15, center=np.array([0.6, -0.05, 0.3])
         )
         return pointcloud
+
+    def get_unprocessed_clay(self, pc2, pc3, pc4, pc5, no_transformation=False, color='Green'):
+        """
+        Stitches together 4 point clouds from cameras 2, 3, 4, and 5. Then,
+        color thresholds based on the clay color and returns only the 
+        dense clay point cloud as a numpy array.
+        """
+        if not no_transformation:
+            # transform each cloud to world frame with camera calibration transformation matrices
+            pc2.transform(self.camera_transforms[2])
+            pc3.transform(self.camera_transforms[3])
+            pc4.transform(self.camera_transforms[4])
+            pc5.transform(self.camera_transforms[5])
+
+        # combine the point clouds
+        pointcloud = o3d.geometry.PointCloud()
+        pointcloud.points = pc5.points
+        pointcloud.colors = pc5.colors
+        pointcloud.points.extend(pc2.points)
+        pointcloud.colors.extend(pc2.colors)
+        pointcloud.points.extend(pc3.points)
+        pointcloud.colors.extend(pc3.colors)
+        pointcloud.points.extend(pc4.points)
+
+        pointcloud, ind = pointcloud.remove_statistical_outlier(
+            nb_neighbors=20, std_ratio=2.0
+        )  # remove statistical outliers
+        pointcloud = self.remove_stage_grippers(pointcloud)
+        pointcloud = self.remove_background(
+            pointcloud, radius=0.15, center=np.array([0.6, 0.0, 0.15])# np.array([0.6, -0.05, 0.3])
+        )
+
+        # color thresholding
+        pointcloud = self.lab_color_crop(pointcloud, color)
+        pointcloud, ind = pointcloud.remove_radius_outlier(
+            nb_points=20, radius=0.005
+        ) 
+        return np.asarray(pointcloud.points)
+    
+    def get_clay(self, pc2, pc3, pc4, pc5, no_transformation=False, color='Green'):
+        """
+        Stitches together 4 point clouds from cameras 2, 3, 4, and 5. Then,
+        color thresholds based on the clay color and returns only the 
+        dense clay point cloud.
+        """
+        if not no_transformation:
+            # transform each cloud to world frame
+            pc2.transform(self.camera_transforms[2])
+            pc3.transform(self.camera_transforms[3])
+            pc4.transform(self.camera_transforms[4])
+            pc5.transform(self.camera_transforms[5])
+
+        # combine the point clouds
+        pointcloud = o3d.geometry.PointCloud()
+        pointcloud.points = pc5.points
+        pointcloud.colors = pc5.colors
+        pointcloud.points.extend(pc2.points)
+        pointcloud.colors.extend(pc2.colors)
+        pointcloud.points.extend(pc3.points)
+        pointcloud.colors.extend(pc3.colors)
+        pointcloud.points.extend(pc4.points)
+        pointcloud.colors.extend(pc4.colors)
+
+        # # bgr --> rgb
+        # full_scene = pointcloud
+        # color_array = np.asarray(full_scene.colors)
+        # print("\nColor Array Shape: ", color_array.shape)
+        # flipped_colors = np.concatenate([np.expand_dims(color_array[:,2], axis=1), np.expand_dims(color_array[:,1],axis=1), np.expand_dims(color_array[:,0], axis=1)], axis=1)
+        # print("\nFlipped Color Shape: ", flipped_colors.shape)
+        # full_scene.colors = o3d.utility.Vector3dVector(flipped_colors)
+        # o3d.visualization.draw_geometries([full_scene]) # TODO: delete after getting the necessary images
+
+        pointcloud, ind = pointcloud.remove_statistical_outlier(
+            nb_neighbors=20, std_ratio=2.0
+        )  # remove statistical outliers
+        pointcloud = self.remove_stage_grippers(pointcloud)
+        pointcloud = self.remove_background(
+            pointcloud, radius=0.15, center=np.array([0.6, 0.0, 0.15])# np.array([0.6, -0.05, 0.3])
+        )
+        
+
+        # color thresholding
+        pointcloud = self.lab_color_crop(pointcloud, color)
+        pointcloud, ind = pointcloud.remove_radius_outlier(
+            nb_points=20, radius=0.005
+        ) 
+
+        # get shape of clay base
+        pointcloud.estimate_normals()
+        downpdc = pointcloud.voxel_down_sample(voxel_size=0.0025)
+        downpdc_points = np.asarray(downpdc.points)
+
+        polygon_indices = np.where(downpdc_points[:, 2] < 0.075) 
+        polygon_pcl = o3d.geometry.PointCloud()
+        polygon_pcl.points = o3d.utility.Vector3dVector(downpdc_points[polygon_indices])
+
+        # generate a 2d grid of points for the base
+        base_plane = []
+        minx, maxx = np.amin(downpdc_points[:, 0]), np.amax(downpdc_points[:, 0])
+        miny, maxy = np.amin(downpdc_points[:, 1]), np.amax(downpdc_points[:, 1])
+        minz, maxz = np.amin(downpdc_points[:, 2]), np.amax(downpdc_points[:, 2])
+        x_vals = np.linspace(minx, maxx, 60)
+        y_vals = np.linspace(miny, maxy, 60)
+        xx, yy = np.meshgrid(
+            x_vals, y_vals
+        )  # create grid that covers full area of 2d polygon
+
+        z = 0.0675
+        zz = np.ones(len(xx.flatten())) * z
+        points = np.vstack((xx.flatten(), yy.flatten(), zz)).T
+
+        grid_cloud = o3d.geometry.PointCloud()
+        grid_cloud.points = o3d.utility.Vector3dVector(points)
+
+        # crop shape of clay base out of 2d grid of points
+        polygon_coords = np.asarray(polygon_pcl.points)[:, 0:2]
+        polygon = Polygon(polygon_coords)
+        mask = [
+            polygon.contains(Point(x, y))
+            for x, y in np.asarray(grid_cloud.points)[:, 0:2]
+        ]
+        cropped_grid = np.asarray(grid_cloud.points)[:, 0:2][mask]
+        zs = np.ones(len(cropped_grid)) * z
+        cropped_grid = np.concatenate(
+            (cropped_grid, np.expand_dims(zs, axis=1)), axis=1
+        )
+
+        base_cloud = o3d.geometry.PointCloud()
+        base_cloud.points = o3d.utility.Vector3dVector(cropped_grid)
+
+        # add top part of clay to new base
+        base_cloud.points.extend(downpdc.points)
+        cropped_plane, ind = base_cloud.remove_statistical_outlier(
+            nb_neighbors=20, std_ratio=2.0
+        )
+
+        base_cloud.colors = o3d.utility.Vector3dVector(
+            np.tile(np.array([0, 0, 1]), (len(base_cloud.points), 1))
+        )
+
+        points = np.asarray(base_cloud.points)
+        pc_center = base_cloud.get_center() 
+        return points, pc_center
     
     def unnormalize_fuse_point_clouds(self, pc2, pc3, pc4, pc5, no_transformation=False, color='Green'):
         """
